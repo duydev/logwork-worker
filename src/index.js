df5576a6743b5ec531e0dd237a6acff844414b59
Trần Nhật Duy <duytn.hcm@gmail.com>
2019-10-28 16:16:20 +0700

    Chore

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const JiraService = require('./jira-service');
+const nodeSchedule = require('node-schedule');
 
 const jiraService = new JiraService();
 
-jiraService
-  .listWorkLogsByIssueIdOrKey('SCINFLUENCER-1742')
-  .then(data => {
-    console.log(data);
-  })
-  .then(() => {
-    return jiraService
-      .addLogWork(
-        'SCINFLUENCER-1742',
-        '1h',
-        new Date(),
-        '- Improve unit test coverage.'
-      )
-      .then(data => {
-        console.log(data);
-      });
-  })
-  .catch(err => {
-    console.log(err);
-  });
+const job = nodeSchedule.scheduleJob(process.env.JOB_SCHEDULE_RULE, () => {
+  jiraService
+    .addLogWork(
+      process.env.JOB_ISSUE_ID_OR_KEY,
+      process.env.JOB_TIMESPENT,
+      new Date(),
+      process.env.JOB_COMMENT
+    )
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
